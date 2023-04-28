04a8abac0a2c065add7643833e277f9433998e09
muxalik <muxail1327@mail.ru>
2023-04-28 11:55:58 +0300

    feature(database) simple structure

---
+++ database/migrations/2023_04_28_083121_create_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Enums\OrderStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->integer('price')->unsigned();
+            $table->enum('status', OrderStatus::values())->default(OrderStatus::Pending->value);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
