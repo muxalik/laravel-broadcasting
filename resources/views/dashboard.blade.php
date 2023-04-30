2bfaef5075bdda1ec2c877c6b106529a4f67e9ab
muxalik <muxail1327@mail.ru>
2023-04-30 21:12:54 +0300

    feature(resource) frontend connection init, channels listening

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,22 +1,17 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex justify-around">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex justify-between">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg" style="height: 100%;width: 56%;">
                 <div class="p-6 text-gray-900">
                     <div class="flex flex-col">
-                        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div class="o sm:-mx-6 lg:-mx-8">
                             <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
                                 <div class="overflow-hidden">
                                     <h2 class="mb-2 mt-0 text-2xl font-medium leading-tight text-primary">
                                         Orders
                                     </h2>
-                                    <table class="min-w-full text-center text-sm font-light">
+                                    <table class="min-w-full text-center text-sm font-light" id="orders">
                                         <thead class="border-b font-medium dark:border-neutral-500">
                                             <tr>
                                                 <th scope="col" class="px-6 py-4">Id</th>
@@ -26,10 +21,11 @@
                                                 <th scope="col" class="px-6 py-4">Status</th>
                                             </tr>
                                         </thead>
-                                        <tbody>
+                                        <tbody id="orders-body">
                                             @forelse ($orders as $order)
-                                                <tr class="border-b border-primary-200 bg-primary-100 text-neutral-800">
-                                                    <td class="whitespace-nowrap px-6 py-4 font-medium">
+                                                <tr
+                                                    class="border-b border-primary-200 bg-primary-100 text-neutral-800 tr-anim">
+                                                    <td class="whitespace-nowrap px-6 py-4">
                                                         {{ $order->id }}
                                                     </td>
                                                     <td class="whitespace-nowrap px-6 py-4">
@@ -46,7 +42,6 @@
                                                     </td>
                                                 </tr>
                                             @empty
-                                                <p>It's empty...</p>
                                             @endforelse
                                             {{-- <tr
                                                 class="border-b border-neutral-100 bg-neutral-50 text-neutral-800 dark:bg-neutral-50">
@@ -64,33 +59,34 @@
                     </div>
                 </div>
             </div>
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg" style="height: 100%;width: 42%;">
                 <div class="p-6 text-gray-900">
                     <div class="flex flex-col">
-                        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div class="sm:-mx-6 lg:-mx-8">
                             <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
                                 <div class="overflow-hidden">
                                     <h2 class="mb-2 mt-0 text-2xl font-medium leading-tight text-primary">
                                         Products
                                     </h2>
-                                    <table class="min-w-full text-center text-sm font-light">
+                                    <table class="min-w-full text-center text-sm font-light" id="products">
                                         <thead class="border-b font-medium dark:border-neutral-500">
                                             <tr>
                                                 <th scope="col" class="px-6 py-4">Id</th>
                                                 <th scope="col" class="px-6 py-4">Price</th>
                                                 <th scope="col" class="px-6 py-4">Discount</th>
                                                 <th scope="col" class="px-6 py-4">Quantity</th>
-                                                <th scope="col" class="px-6 py-4">Times bought</th>
+                                                <th scope="col" class="px-6 py-4">Bought</th>
                                             </tr>
                                         </thead>
-                                        <tbody>
+                                        <tbody id="products-body">
                                             @forelse ($products as $product)
-                                                <tr class="border-b border-primary-200 bg-primary-100 text-neutral-800">
-                                                    <td class="whitespace-nowrap px-6 py-4 font-medium">
+                                                <tr
+                                                    class="border-b border-primary-200 bg-primary-100 text-neutral-800 tr-anim">
+                                                    <td class="whitespace-nowrap px-6 py-4">
                                                         {{ $product->id }}
                                                     </td>
                                                     <td class="whitespace-nowrap px-6 py-4">
-                                                        {{ formatPrice($product->price) . "$" }}
+                                                        {{ formatPrice(prettyPrice($product->calculatePrice())) . "$" }}
                                                     </td>
                                                     <td class="whitespace-nowrap px-6 py-4">
                                                         {{ $product->discount . '%' }}
@@ -103,7 +99,6 @@
                                                     </td>
                                                 </tr>
                                             @empty
-                                                <p>It's empty...</p>
                                             @endforelse
                                             {{-- <tr
                                                 class="border-b border-neutral-100 bg-neutral-50 text-neutral-800 dark:bg-neutral-50">
@@ -123,4 +118,200 @@
             </div>
         </div>
     </div>
+    <script type="module">
+        function animateRow(tr) 
+        {
+            anime({
+                targets: tr.getElementsByTagName('td'),
+                opacity: [0, 1],
+                scale: [2, 1],
+                easing: 'easeInOutQuad',
+                delay: function(el, i, l) {
+                    return 100 + i * 100;
+                },
+                endDelay: function(el, i, l) {
+                    return (l - i) * 100;
+                },
+            });
+        }
+
+        function animateCellIn(td) 
+        {
+            anime({
+                targets: td,
+                opacity: [1, 0],
+                scale: [1, 1.5],
+                easing: 'easeInQuad',
+                duration: 400,
+            });
+        }
+
+        function animateCellOut(td) 
+        {
+            anime({
+                targets: td,
+                opacity: [0, 1],
+                scale: [1.5, 1],
+                easing: 'easeOutQuad',
+                duration: 400,
+            });
+        }
+
+        function animateDelete(tr)
+        {
+            anime({
+                targets: tr,
+                scale: [1, 0],
+                easing: 'easeInOutQuad',
+                duration: 400,
+            });
+        }
+                
+        Echo.channel('products')
+            .listen('.product.created', (e) => {
+                let body = document.getElementById('products-body');
+                let tr = document.createElement('tr');
+
+                tr.classList.add('border-b', 'border-primary-200', 'bg-primary-100', 'text-neutral-800', 'tr-anim', 'tr-animating');
+                tr.innerHTML = '<td class="whitespace-nowrap px-6 py-4">'
+                    + +e.product.id + 
+                    '</td>' + 
+                    '<td class="whitespace-nowrap px-6 py-4">'
+                        + e.product.price + '$' + 
+                        '</td>' +
+                    '<td class="whitespace-nowrap px-6 py-4">'
+                        + e.product.discount + '%' +
+                    '</td>' +
+                    '<td class="whitespace-nowrap px-6 py-4">'
+                        + e.product.quantity + 
+                    '</td>' +
+                    '<td class="whitespace-nowrap px-6 py-4">'
+                        + e.product.times_bought +
+                    '</td>';
+
+                body.append(tr);
+
+                animateRow(tr);
+
+                setTimeout(() => {
+                    tr.classList.remove('tr-animating');
+                }, 500);
+            }).listen('.product.bought', (e) => {
+                let trs = document.getElementById('products-body').getElementsByTagName('tr');
+
+                [...trs].every(tr => {
+                    let children = [...tr.getElementsByTagName('td')];      
+
+                    if (children[0].textContent == e.product.id) {
+                        animateCellIn(children[children.length - 1]);
+                        setTimeout(() => {
+                            children[children.length - 1].textContent = e.product.times_bought;
+                            animateCellOut(children[children.length - 1]);
+                        }, 400);
+                        
+
+                        animateCellIn(children[children.length - 2]);
+                        setTimeout(() => {
+                            children[children.length - 2].textContent = e.product.quantity;
+                            animateCellOut(children[children.length - 2]);
+                        }, 400);
+
+                        return false;
+                    }
+
+                    return true;
+                });
+            }).listen('.product.price.changed', (e) => {
+                let trs = document.getElementById('products-body').getElementsByTagName('tr');
+
+                [...trs].every(tr => {
+                    let children = [...tr.getElementsByTagName('td')];      
+
+                    if (children[0].textContent == e.product.id) {
+                        animateCellIn(children[1]);
+                        setTimeout(() => {
+                            children[1].textContent = e.product.price + "$";
+                            animateCellOut(children[1]);
+                        }, 400);
+
+                        animateCellIn(children[2]);
+                        setTimeout(() => {
+                            children[2].textContent = e.product.discount + "%";
+                            animateCellOut(children[2]);
+                        }, 400);
+
+                        return false;
+                    }
+
+                    return true;
+                });
+            });
+
+        Echo.channel('orders')
+            .listen('.order.created', (e) => {
+                let body = document.getElementById('orders-body');
+                let tr = document.createElement('tr');
+
+                tr.classList.add('border-b', 'border-primary-200', 'bg-primary-100', 'text-neutral-800', 'tr-anim', 'tr-animating');
+                tr.innerHTML = `<td class="whitespace-nowrap px-6 py-4">
+                        ` + e.order.id + `
+                    </td>
+                    <td class="whitespace-nowrap px-6 py-4">
+                        ` + e.order.username + `
+                    </td>
+                    <td class="whitespace-nowrap px-6 py-4">
+                        ` + e.order.price + `$
+                    </td>
+                    <td class="whitespace-nowrap px-6 py-4">
+                        ` + e.order.quantity + `
+                    </td>
+                    <td class="whitespace-nowrap px-6 py-4">
+                        ` + e.order.status + `
+                    </td>`;
+
+                body.append(tr);
+
+                animateRow(tr);
+
+                setTimeout(() => {
+                    tr.classList.remove('tr-animating');
+                }, 500);
+            }).listen('.order.status.changed', (e) => {
+                let trs = document.getElementById('orders-body').getElementsByTagName('tr');
+
+                [...trs].every(tr => {
+                    let children = [...tr.getElementsByTagName('td')];      
+
+                    if (children[0].textContent == e.order.id) {
+                        animateCellIn(children[children.length - 1]);
+                        setTimeout(() => {
+                            children[children.length - 1].textContent = e.order.status;
+                            animateCellOut(children[children.length - 1]);
+                        }, 400);
+
+                        return false;
+                    }
+
+                    return true;
+                });
+            }).listen('.order.deleted', (e) => {
+                let trs = document.getElementById('orders-body').getElementsByTagName('tr');
+
+                [...trs].every(tr => {
+                    let children = [...tr.getElementsByTagName('td')];      
+
+                    if (children[0].textContent == e.id) {
+                        animateDelete(tr);
+                        setTimeout(() => {
+                            tr.remove();
+                        }, 400);
+
+                        return false;
+                    }
+
+                    return true;
+                });
+            });
+
+    </script>
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,9 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js" integrity="sha512-z4OUqw38qNLpn1libAN9BsoDx6nbNFio5lA6CuTp9NlK83b89hgyCVq+N5FdBJptINztxn1Z3SaKSKUS5UP60Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
